1db934051c8b153885368424c4a96d4a3b01232e
Alex Lisenkov <alex@create.nl>
2018-12-17 20:44:01 +0100

    Removed config.ts

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-export const FIREBASE_CONFIG = {
-    // Put your Firebase config here.
-    apiKey: "AIzaSyAnwepQ0dkoXU-LSk4tChWCM5uDmunwzuE",
-    authDomain: "react-ts-b9413.firebaseapp.com",
-    databaseURL: "https://react-ts-b9413.firebaseio.com",
-    messagingSenderId: "199780514361",
-    projectId: "react-ts-b9413",
-    storageBucket: "",
-};
